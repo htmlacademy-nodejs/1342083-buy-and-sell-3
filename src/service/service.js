689a432b140969677136974e2e134cbb4b4b822d
Nick Petrenko <extendsnull@gmail.com>
2021-04-03 16:30:16 +0300

    Интегрирует генератор

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+  ExitCode,
+} = require(`../constants`);
+
+const userArgumens = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArgumens;
+
+if (userArgumens.length === 0 || !Cli[userCommand]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[userCommand].run(userArgumens.slice(1));
