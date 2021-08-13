dd74bc0cf91e7738ab703a1f57e388af8ec31c24
extendsnull <extendsnull@gmail.com>
2021-08-13 18:10:33 +0300

    Рефакторит код

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -18,29 +18,22 @@ const {
   MocksConfig,
   OfferType
 } = require(`./constants`);
-const {
-  DEFAULT_COUNT,
-  DESCRIPTION_RESTRICT,
-  FILE_NAME,
-  PICTURE_INDEX_RESTRICT,
-  SUM_RESTRICT
-} = MocksConfig;
 
 const getRandomId = () => nanoid(MocksConfig.MAX_ID_LENGTH);
 
 const getRandomTitle = (titles) => getRandomArrayItem(titles);
 
 const getRandomPicture = () => {
-  const index = getRandomInt(PICTURE_INDEX_RESTRICT.MIN, PICTURE_INDEX_RESTRICT.MAX).toString().padStart(2, `0`);
+  const index = getRandomInt(MocksConfig.PICTURE_INDEX_RESTRICT.MIN, MocksConfig.PICTURE_INDEX_RESTRICT.MAX).toString().padStart(2, `0`);
 
   return `item${index}.jpg`;
 };
 
-const getRandomText = (sentences) => getRandomArrayItems(sentences, DESCRIPTION_RESTRICT.MAX).join(` `);
+const getRandomText = (sentences) => getRandomArrayItems(sentences, MocksConfig.DESCRIPTION_RESTRICT.MAX).join(` `);
 
 const getType = () => getRandomArrayItem(Object.values(OfferType));
 
-const getRandomSum = () => getRandomInt(SUM_RESTRICT.MIN, SUM_RESTRICT.MAX);
+const getRandomSum = () => getRandomInt(MocksConfig.SUM_RESTRICT.MIN, MocksConfig.SUM_RESTRICT.MAX);
 
 const getCategories = (categories) => getRandomArrayItems(categories);
 
@@ -87,12 +80,12 @@ module.exports = {
     const comments = await readContent(FilePath.COMMENTS);
 
     const [count] = args;
-    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    const countOffer = Number.parseInt(count, 10) || MocksConfig.DEFAULT_COUNT;
     const mocks = generateOffer(countOffer, titles, sentences, categories, comments);
     const content = JSON.stringify(mocks, null, 2);
 
     try {
-      await fs.writeFile(FILE_NAME, content);
+      await fs.writeFile(MocksConfig.FILE_NAME, content);
       console.info(chalk.green(`Операция успешна. Файл создан.`));
       process.exit(ExitCode.SUCCESS);
     } catch (err) {
